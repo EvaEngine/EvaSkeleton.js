621ddf252a642b8808759dc808a78da8bb74a090
AlloVince <allo.vince@gmail.com>
2016-06-23 16:26:46 +0800

    upgrade engine

--- test/bootstrap.js
+++ test/bootstrap.js
@@ -1,8 +1,5 @@
-import { EvaEngine, DI } from 'evaengine';
-import httpMocker from 'node-mocks-http';
-import EventEmitter from 'events';
+import { EvaEngine, DI, utils } from 'evaengine';
 import chai from 'chai';
-import assert from 'assert';
 
 const engine = new EvaEngine({
   projectRoot: `${__dirname}/..`,
@@ -14,46 +11,6 @@ global.p = (...args) => {
   DI.get('logger').debug(...args);
 };
 
-module.exports.truncateAll = async(entities) => {
-  const names = [];
-  const allEntities = entities.getAll();
-  for (const name in allEntities) {
-    if (typeof allEntities[name].truncate === 'function' && allEntities[name].tableName) {
-      names.push(`TRUNCATE \`${allEntities[name].tableName}\`;\n`);
-    }
-  }
-  await entities.getInstance().query(names.join(''));
-};
-
-const mockResponse = () => httpMocker.createResponse({ eventEmitter: EventEmitter });
-module.exports.mockResponse = mockResponse;
-module.exports.mockRequest = (...args) => httpMocker.createRequest(...args);
-module.exports.mockAuthRequest = (...args) => {
-  const { params: params = {} } = args[0];
-  params.api_key = DI.get('config').get('token.faker.key');
-  args[0].params = params;
-  return httpMocker.createRequest(...args);
-};
-module.exports.httpMocker = httpMocker;
-
-// module.exports.assert = assert;
-chai.should();
-module.exports.assert = chai.assert;
-
-
-const runController = async(controller, request, response = mockResponse()) =>
-  new Promise((resolve, reject) => {
-    controller.handle(request, response, err => reject(err));
-    response.on('end', () => resolve(JSON.parse(response._getData())));
-  });
-module.exports.runController = runController;
-module.exports.mochaAsync = (fn) => {
-    return async (done) => {
-        try {
-            await fn();
-            done();
-        } catch (err) {
-            done(err);
-        }
-    };
-};
+module.exports = Object.assign(utils.test, {
+  assert: chai.assert
+});
--- test/routes/api/hello_world.js
+++ test/routes/api/hello_world.js
@@ -4,7 +4,7 @@ import authController from '../../../src/routes/api/hello_world';
 import { assert, mochaAsync, mockRequest, runController } from '../../bootstrap';
 
 describe('Demo API', () => {
-  it('Could login success', mochaAsync(async() => {
+  it('Could login success', async() => {
     const res = await runController(authController, mockRequest({
       method: 'POST', url: '/login', body: {
         username: 'evaengine',
@@ -12,18 +12,6 @@ describe('Demo API', () => {
       }
     }));
     assert.isString(res.token);
-  }));
+  });
 
-  it.skip('Login fail when wrong password', mochaAsync(async() => {
-    try {
-      await runController(authController, mockRequest({
-        method: 'POST', url: '/login', body: {
-          username: 'evaengine',
-          password: ''
-        }
-      }));
-    } catch (err) {
-      assert.instanceOf(err, exceptions.InvalidArgumentException);
-    }
-  }));
 });
