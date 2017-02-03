d5a94b6dc12619309be6e997b1470e42145f7196
AlloVince <allo.vince@gmail.com>
2017-02-03 22:58:55 +0800

    add models

---
+++ src/models/index.js
@@ -0,0 +1,18 @@
+import fs from 'fs';
+import path from 'path';
+
+const models = {};
+
+fs
+  .readdirSync(__dirname)
+  .filter((file) => {
+    const fileArray = file.split('.');
+    return (file.indexOf('.') !== 0) &&
+      (['js', 'es6'].indexOf(fileArray.pop()) !== -1) && (fileArray[0] !== 'index');
+  })
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file)); //eslint-disable-line
+    models[model.default.name] = model.default;
+  });
+
+export default models;
